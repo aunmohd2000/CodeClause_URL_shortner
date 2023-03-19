b3ee8308cf073147618c2dfd3e850780c60d95c3
Syed Aun Mohd <aunmohd280@gmail.com>
2023-03-19 23:30:09 +0530

    commit

--- Urlshort.py
+++ Urlshort.py
@@ -1,6 +1,6 @@
 import pyshorteners  # import the library
 
-url = input('Enter the url: ')   # take input from the user i.e. the url which is to be converted into short url
+url = input('Enter the long url: ')   # take input from the user i.e. the url which is to be converted into short url
 
 # creating a function to convert the long url into short
 # NOTE - it can also be done without a function but functions make a program easily understandable and editable
@@ -10,7 +10,7 @@ def shortenurl(url):
     print(s.tinyurl.short(url))   
 
 
-shortenurl(url) # calling the shortenurl function taht we just created also passing long url as parameter
+shortenurl(url) # calling the shortenurl function that we just created also passing long url as parameter
 
 a = input("Press Any Key to Quit")  
 
